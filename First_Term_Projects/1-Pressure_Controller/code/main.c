2d7d18cca004b99275e9dd9a627ca5a2e79e18d2
Mahmoud Ayoub <84600209+Mahmoud-Ayoub@users.noreply.github.com>
2021-08-25 22:49:12 +0200

    Update main.c

--- First_Term_Projects/1-Pressure_Controller/code/main.c
+++ First_Term_Projects/1-Pressure_Controller/code/main.c
@@ -1,1 +1,23 @@
+#include "driver.h"
+#include "pressure_sensor.h"
+#include "PC_Algorithm.h"
+#include "Alarm.h"
+
+int main (){
+	
+	int p_value ; 
+	extern int pressure_threshold ; 
+
+	GPIO_INITIALIZATION();
+	while (1)
+	{
+		p_value = getPressureVal() ; 
+		if (p_value > pressure_threshold) {
+			Alarm_ON () ; 
+		}
+		else if (p_value <= pressure_threshold) {
+			Alarm_OFF () ; 
+		}	
+	}
+}
 
