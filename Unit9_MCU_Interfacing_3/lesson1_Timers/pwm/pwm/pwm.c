3de8f56649f8c58b2d9caabddfc85219ebd58438
Mahmoud Ayoub <84600209+Mahmoud-Ayoub@users.noreply.github.com>
2021-11-17 23:19:17 +0200

    fixed indentation issue

--- Unit9_MCU_Interfacing_3/lesson1_Timers/pwm/pwm/pwm.c
+++ Unit9_MCU_Interfacing_3/lesson1_Timers/pwm/pwm/pwm.c
@@ -7,9 +7,9 @@
 
 /*
 	@Fn		: MCAL_Pwm_Init
-	@brief	: initialize timer 0 with the required PWM specifications 
-	@inputs : pwm configuration
-	@outputs : none
+	@brief		: initialize timer 0 with the required PWM specifications 
+	@inputs 	: pwm configuration
+	@outputs 	: none
 */
 void MCAL_Pwm_Init (PWM_Config_t* pwm_config) {
 	T0_Control_REG |= (pwm_config->pwm_mode) | (pwm_config->output_compare_mode) | (pwm_config->clock_select) ; 
@@ -17,9 +17,9 @@ void MCAL_Pwm_Init (PWM_Config_t* pwm_config) {
 
 /*
 	@Fn		: MCAL_Pwm_Start
-	@brief	: Start generating pwm waveform on OC0 pin with the required duty cycle
-	@inputs : duty cycle
-	@outputs : none
+	@brief		: Start generating pwm waveform on OC0 pin with the required duty cycle
+	@inputs 	: duty cycle
+	@outputs 	: none
 */
 void MCAL_Pwm_Start (uint8 duty_cycle) {
 	uint8 outcmp_value = 255 * duty_cycle / 100 ;
